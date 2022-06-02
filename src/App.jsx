6a84fcee2eafb7c4bbbdc8049f7419a51e5cb4e0
IgnacioCalvagna <ignaciocalvagna@gmail.com>
2022-06-02 20:16:42 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <strong>Hola mundo... Soy Coder-app</strong>
+          </div>
+          
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;

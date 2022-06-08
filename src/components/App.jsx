c4d8ebaec7a199fd489040457a2d8b7bdfde40d3
IgnacioCalvagna <ignaciocalvagna@gmail.com>
2022-06-08 20:39:07 -0300

    Agregando Navbar utilizando Material_Ui y adaptando al proyecto

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "../assets/css/App.css";
+import Card from "./Card";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <>
       <div className="App">
+        <Navbar />
+
         <header className="App-header">
           <div>
             <strong>Hola mundo... Soy Coder-app</strong>
           </div>
-          
+
+          <div className="miCard">
+            <Card />
+          </div>
         </header>
       </div>
     </>
